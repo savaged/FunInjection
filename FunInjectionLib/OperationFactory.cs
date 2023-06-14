030e917b6f5cd3f2f923653fcea796f1090973fb
savaged <savagedavid@gmail.com>
2023-06-14 15:43:40 +0100

    working

--- FunInjectionLib/OperationFactory.cs
+++ FunInjectionLib/OperationFactory.cs
@@ -7,12 +7,20 @@ internal static class OperationFactory
     public static Operation Get(string operationName)
     {
         operationName = operationName?.FirstCharToUpper() ?? string.Empty;
-        switch (operationName)
+        return operationName switch
         {
-            case nameof(Operations.Sum):
-                return Operations.Sum;
-            default:
-                return Operations.Zero;
-        }
+            nameof(Operations.Sum) => Operations.Sum,
+            nameof(Operations.Add) => Operations.Add,
+            nameof(Operations.Sub) => Operations.Sub,
+            nameof(Operations.Mult) => Operations.Mult,
+            nameof(Operations.Avg) => Operations.Avg,
+            nameof(Operations.Mean) => Operations.Mean,
+            nameof(Operations.Max) => Operations.Max,
+            nameof(Operations.Min) => Operations.Min,
+            nameof(Operations.Inv) => Operations.Inv,
+            nameof(Operations.Incr) => Operations.Incr,
+            nameof(Operations.Decr) => Operations.Decr,
+            _ => Operations.Zero
+        };
     }
 }
