60d13756b16bc4f5746844f7f05ad9146ad1d5da
savaged <savagedavid@gmail.com>
2023-06-15 08:52:05 +0100

    refactored

--- FunInjectionLib/Extensions.cs
+++ FunInjectionLib/Extensions.cs
@@ -4,14 +4,10 @@ namespace FunInjectionLib;
 
 public static class Extensions
 {
-    public static int[] ToInts(this IEnumerable<string> self)
-    {
-        var list = new List<int>();
-        foreach (var s in self)
-        {
-            if (int.TryParse(s, out var i))
-                list.Add(i);
-        }
-        return list.ToArray();
-    }
+    public static int[] ToInts(this IEnumerable<string> self) =>
+        self.Select(s => {
+                if (int.TryParse(s, out var i))
+                    return i;
+                return 0;
+                }).Where(i => i != 0).ToArray();
 }
