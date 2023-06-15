0f7ca22c8600407f900873cd7083b0207294d5e7
savaged <savagedavid@gmail.com>
2023-06-15 09:54:02 +0100

    working

--- FunInjectionLib/OperationService.cs
+++ FunInjectionLib/OperationService.cs
@@ -6,8 +6,8 @@ public static class OperationService
 {
     public static bool IsValid(string[] args) =>
         args.Length > 1 && Regex.IsMatch(args[0], "[a-z]{3,}");
-    
+
     public static Func<int[], int> Get(string[] args) => OperationFactory.Get(args[0]);
-    
+
     public static int Run(Func<int[], int> operation, int[] operands) => operation(operands);
 }
--- FunInjectionTests/UnitTests.cs
+++ FunInjectionTests/UnitTests.cs
@@ -3,7 +3,13 @@ namespace FunInjectionTests;
 public class UnitTests
 {
     [Fact]
-    public void TestIsValid()
+    public void TestFeedbackServiceIsValid()
+    {
+        FeedbackService.ForOperation("sum", 10).Should().BeEquivalentTo("sum: 10");
+    }
+
+    [Fact]
+    public void TestOperationServiceIsValid()
     {
         OperationService.IsValid("sum 2 3 4 1".ToArgs()).Should().BeTrue();
         OperationService.IsValid("sum 2 3 4 x 1".ToArgs()).Should().BeTrue();
